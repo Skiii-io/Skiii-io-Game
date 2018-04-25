80564332f604f4a9c3de41ad3b76b4b4dc39343e
Jordan Painter <pai8264@waingels.wokingham.sch.uk>
2018-04-25 09:59:13 +0100

    Added skier with iffy controls
    Not gonna lie the controls aren't that good

---
+++ Skiii-io/Assets/SkierController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using UnityEngine;
+
+public class SkierController : MonoBehaviour
+{
+
+
+	public float speed;
+	private Rigidbody2D rb2d;
+
+	private float downSpeed;
+	private float currentGravity;
+
+	public bool GoingLeft;
+	public bool GoingRight;
+
+	void Start()
+	{
+		downSpeed = rb2d.velocity.y;
+		currentGravity = rb2d.gravityScale;
+	}
+	
+	void FixedUpdate()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+		if (StartTextDisplay.StartGame)
+		{
+			if (Input.GetKeyDown(KeyCode.A))
+			{
+				MoveLeft();
+			}
+			else if (Input.GetKeyDown(KeyCode.D))
+			{
+				MoveRight();
+			}
+			else if (Input.GetKeyDown(KeyCode.S))
+			{
+				MoveDown();
+			}
+		}
+	}
+
+	void MoveLeft()
+	{
+		Vector3 LeftMovement = new Vector3(-20f, downSpeed, 0f);
+
+		rb2d.AddForce(LeftMovement * speed);
+
+		rb2d.gravityScale = currentGravity;
+		
+		GoingLeft = true;
+		GoingRight = false;
+	}
+
+	void MoveRight()
+	{
+		Vector3 RightMovement = new Vector3(20f, downSpeed, 0f);
+
+		rb2d.AddForce(RightMovement * speed);
+		
+		rb2d.gravityScale = currentGravity;
+
+		GoingRight = true;
+		GoingLeft = false;
+	}
+
+	void MoveDown()
+	{
+		Vector3 DownMovement = new Vector3(0f, 0f, 0f);
+
+		GoingLeft = false;
+		GoingRight = false;
+		
+		
+	}
+}
---
+++ Skiii-io/Assets/SkierSpriteChange.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkierSpriteChange : MonoBehaviour
+{
+
+	private SpriteRenderer sprd;
+
+	public Sprite SpriteDown;
+	public Sprite SpriteRight;
+	public Sprite SpriteLeft;
+
+	void Start()
+	{
+		sprd = GetComponent<SpriteRenderer>();
+	}
+	
+	void Update()
+	{
+		if (StartTextDisplay.StartGame)
+		{
+			if (GameObject.FindGameObjectWithTag("Skier").GetComponent<SkierController>().GoingLeft)
+			{
+				sprd.sprite = SpriteLeft;
+			}
+
+			else if (GameObject.FindGameObjectWithTag("Skier").GetComponent<SkierController>().GoingRight)
+			{
+				sprd.sprite = SpriteRight;
+			}
+			else if (GameObject.FindGameObjectWithTag("Skier").GetComponent<SkierController>().GoingLeft == false && GameObject.FindGameObjectWithTag("Skier").GetComponent<SkierController>().GoingRight == false)
+			{
+				sprd.sprite = SpriteDown;
+			}
+		}
+	}
+	
+	
+	
+}
